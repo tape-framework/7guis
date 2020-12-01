78b90ea8b2552cb9538db06409ed2fe899067614
clyfe <>
2020-12-01 18:01:50 +0200

    Add counter gui

--- src/guis7/app/guis/counter/view.cljs
+++ src/guis7/app/guis/counter/view.cljs
@@ -1,11 +1,18 @@
 (ns guis7.app.guis.counter.view
-  (:require [tape.mvc.view :as v :include-macros true]
-            [guis7.app.guis.counter.controller]))
+  (:require [re-frame.core :as rf]
+            [tape.mvc.view :as v :include-macros true]
+            [tape.router :as router]
+            [guis7.app.guis.counter.controller :as counter.c]))
 
 ;;; Views
 
 (defn ^::v/view index []
-  [:p "Counter"])
+  (let [cnt @(rf/subscribe [::counter.c/count])]
+    [:div.field.has-addons
+     [:div.control
+      [:input.input {:type "number" :value cnt :read-only true}]]
+     [:div.control
+      [:a.button.is-info {:href (router/href [::counter.c/increment])} "Count"]]]))
 
 ;;; Module
 
