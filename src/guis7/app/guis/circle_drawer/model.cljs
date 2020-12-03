2a98958d42849302b16ecf8d721b8ffac41a5055
clyfe <>
2020-12-03 20:13:50 +0200

    Add circle drawer gui

---
+++ src/guis7/app/guis/circle_drawer/model.cljs
@@ -0,0 +1,64 @@
+(ns guis7.app.guis.circle-drawer.model)
+
+(defn insert [circles circle]
+  (let [id (->> circles keys (apply max) inc)]
+    (assoc circles id circle)))
+
+(defn- distance [x1 y1 x2 y2]
+  (let [dx (Math/abs (- x1 x2))
+        dy (Math/abs (- y1 y2))]
+    (Math/sqrt (+ (* dx dx) (* dy dy)))))
+
+(defn select
+  "Returns the key of the circle closes to the mouse, if the mouse is over it."
+  [circles x y]
+  (let [over?   (fn [[_ [cx cy r]]] (< (distance cx cy x y) r))
+        closest (fn
+                  ([] nil)
+                  ([c] c)
+                  ([c1 c2] (let [[_ [cx1 cy1]] c1
+                                 [_ [cx2 cy2]] c2
+                                 d1 (distance cx1 cy1 x y)
+                                 d2 (distance cx2 cy2 x y)]
+                             (if (< d1 d2) c1 c2))))]
+    (->> circles
+         (filter over?)
+         (reduce closest nil)
+         first)))
+
+(defn- ckw [name]
+  (keyword "guis7.app.guis.circle-drawer.controller" name))
+
+(def init {(ckw :snapshots)  [{} {1 [100 100 30]}]
+           (ckw :snapshot-i) 1
+           (ckw :undo?)      true
+           (ckw :redo)       false
+           (ckw :circles)    {1 [100 100 30]}})
+  
+(defn snapshot [db]
+  (let [snapshot-i (inc ((ckw :snapshot-i) db))
+        circles    ((ckw :circles) db)]
+    (-> db
+        (assoc (ckw :undo?) true)
+        (assoc (ckw :redo?) false)
+        (update (ckw :snapshots) assoc snapshot-i circles)
+        (update (ckw :snapshots) subvec 0 (inc snapshot-i))
+        (assoc (ckw :snapshot-i) snapshot-i))))
+
+(defn undo [db]
+  (let [snapshots  ((ckw :snapshots) db)
+        snapshot-i (dec ((ckw :snapshot-i) db))]
+    (assoc db
+      (ckw :undo?) (< 0 snapshot-i)
+      (ckw :redo?) true
+      (ckw :circles) (get snapshots snapshot-i)
+      (ckw :snapshot-i) snapshot-i)))
+
+(defn redo [db]
+  (let [snapshots  ((ckw :snapshots) db)
+        snapshot-i (inc ((ckw :snapshot-i) db))]
+    (assoc db
+      (ckw :undo?) true
+      (ckw :redo?) (< snapshot-i (dec (count snapshots)))
+      (ckw :circles) (get snapshots snapshot-i)
+      (ckw :snapshot-i) snapshot-i)))
--- src/guis7/app/guis/circle_drawer/view.cljs
+++ src/guis7/app/guis/circle_drawer/view.cljs
@@ -1,11 +1,60 @@
 (ns guis7.app.guis.circle-drawer.view
-  (:require [tape.mvc.view :as v :include-macros true]
-            [guis7.app.guis.circle-drawer.controller]))
+  (:require [re-frame.core :as rf]
+            [tape.mvc.view :as v :include-macros true]
+            [guis7.app.guis.circle-drawer.controller :as circle-drawer.c]))
+
+;;; Helpers
+
+(defn- create [event]
+  (let [rect (.getBoundingClientRect (.-currentTarget event))
+        x    (- (.-clientX event) (.-left rect))
+        y    (- (.-clientY event) (.-top rect))]
+    (rf/dispatch [::circle-drawer.c/create x y])))
+
+(defn- select [event]
+  (let [rect (.getBoundingClientRect (.-currentTarget event))
+        x    (- (.-clientX event) (.-left rect))
+        y    (- (.-clientY event) (.-top rect))]
+    (rf/dispatch [::circle-drawer.c/select x y])))
 
 ;;; Views
 
 (defn ^::v/view index []
-  [:p "Circle drawer"])
+  (let [circles    @(rf/subscribe [::circle-drawer.c/circles])
+        selected   @(rf/subscribe [::circle-drawer.c/selected])
+        activated? @(rf/subscribe [::circle-drawer.c/activated?])
+        editing?   @(rf/subscribe [::circle-drawer.c/editing?])
+        undo?      @(rf/subscribe [::circle-drawer.c/undo?])
+        redo?      @(rf/subscribe [::circle-drawer.c/redo?])
+        activate   (fn [event]
+                     (rf/dispatch [::circle-drawer.c/activate])
+                     (.preventDefault event))]
+    [:<>
+     [:div.field.is-grouped
+      [:p.control
+       [:button.button {:disabled (not undo?)
+                        :on-click #(when undo? (rf/dispatch [::circle-drawer.c/undo]))} "Undo"]]
+      [:p.control
+       [:button.button {:disabled (not redo?)
+                        :on-click #(when redo? (rf/dispatch [::circle-drawer.c/redo]))} "Redo"]]]
+     [:div.circle-drawer {:on-click create :on-mouse-move select}
+      (for [[i [x y r]] circles :let [d (* 2 r)]]
+        [:div.circle {:key             i,
+                      :class           (when (= i selected) "is-selected")
+                      :on-context-menu (when (= i selected) activate)
+                      :style           {:left (- x r) :top (- y r) :width d :height d}}])]
+     [:div.modal {:class (when activated? "is-active")}
+      [:div.modal-background]
+      [:div.modal-content
+       [:button.button.is-primary {:on-click #(rf/dispatch [::circle-drawer.c/edit])} "Adjust diameter..."]]
+      [:button.modal-close.is-large {:on-click #(rf/dispatch [::circle-drawer.c/deactivate])}]]
+     [:div.modal {:class (when editing? "is-active")}
+      [:div.modal-background]
+      [:div.modal-content
+       [:input {:type      "range" :step 1 :min 0 :max 100
+                :value     (-> circles (get selected) (get 2))
+                :on-change #(rf/dispatch [::circle-drawer.c/set-radius (-> % .-target .-value js/parseInt)])}]]
+      [:button.modal-close.is-large {:on-click #(rf/dispatch [::circle-drawer.c/stop-edit])}]]]))
 
 ;;; Module
 
